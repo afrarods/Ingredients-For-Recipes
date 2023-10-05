1f0244d7a5a0b63f7afc27772d242d8ffbc029e3
Afonso Rodrigues <afrarods@hotmail.com>
2023-10-05 05:32:31 +0100

    Passing Ing.ts from Recipes to SL via a Service

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,11 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css'],
 })
-export class RecipeDetailComponent {
+export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
+
+  constructor(private recipesService: RecipesService) {}
+
+  ngOnInit() {}
+
+  onAddToShoppingList() {
+    this.recipesService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
