e73db0570cd5b961798325069d088170308b9865
Afonso Rodrigues <afrarods@hotmail.com>
2023-10-05 01:35:30 +0100

    Managing Recipes in a Recipe Service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,27 +9,13 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      'name TestOne',
-      'description TestOne',
-      'https://www.corriecooks.com/wp-content/uploads/2023/01/white-eggplant.jpg'
-    ),
-    new Recipe(
-      'name TestTwo',
-      'description TestTwo',
-      'https://images.deliveryhero.io/image/foodpanda/recipes/igado-recipe-2.jpg'
-    ),
-    new Recipe(
-      'name TestThree',
-      'description TestThree',
-      'https://hostessatheart.com/wp-content/uploads/2022/09/Oven-Brisket-Recipe-SM-Feature-1200x630-Image-735x386.png.webp'
-    ),
-  ];
+  recipes: Recipe[];
 
-  constructor() {}
+  constructor(private recipesService: RecipesService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.recipes = this.recipesService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,1 +1,25 @@
-export class RecipesService {}
+import { Recipe } from './recipe.model';
+
+export class RecipesService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'name TestOne',
+      'description TestOne',
+      'https://www.corriecooks.com/wp-content/uploads/2023/01/white-eggplant.jpg'
+    ),
+    new Recipe(
+      'name TestTwo',
+      'description TestTwo',
+      'https://images.deliveryhero.io/image/foodpanda/recipes/igado-recipe-2.jpg'
+    ),
+    new Recipe(
+      'name TestThree',
+      'description TestThree',
+      'https://hostessatheart.com/wp-content/uploads/2022/09/Oven-Brisket-Recipe-SM-Feature-1200x630-Image-735x386.png.webp'
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
