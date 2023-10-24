694c255c82ee1935db78068409c3f1e0abb1f750
Afonso Rodrigues <afrarods@hotmail.com>
2023-10-24 16:28:57 +0100

    Storing Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RecipesService } from '../recipes/recipes.service';
 
 @Injectable({ providedIn: 'root' })
-export class DataStorageSErvice {
-  constructor(private http: HttpClient) {}
+export class DataStorageService {
+  constructor(
+    private http: HttpClient,
+    private recipesService: RecipesService
+  ) {}
+
+  storeRecipes() {
+    const recipes = this.recipesService.getRecipes();
+    this.http
+      .put(
+        'https://ng-ingredients-for-recipes-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
