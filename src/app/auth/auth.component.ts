4fdc9426c6eac14d774c3dc0586407cd6b45e925
Afonso Rodrigues <afrarods@hotmail.com>
2023-11-03 11:30:26 +0000

    Adding a Loading Spinner & Error Handling Logic

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,6 +9,8 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
   constructor(private authService: AuthService) {}
 
@@ -24,15 +26,19 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
+    this.isLoading = true;
     if (this.isLoginMode) {
       //...
     } else {
       this.authService.signup(email, password).subscribe(
         (responseData) => {
           console.log(responseData);
+          this.isLoading = false;
         },
         (error) => {
           console.log(error);
+          this.error = 'Failed to sign up! Try again later.';
+          this.isLoading = false;
         }
       );
     }
