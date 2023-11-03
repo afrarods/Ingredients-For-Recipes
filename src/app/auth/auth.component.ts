72f606f53fc2808259317d859ad66c420a0975f9
Afonso Rodrigues <afrarods@hotmail.com>
2023-11-03 13:22:37 +0000

    Sending Login Requests

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { AuthResponseData, AuthService } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -26,22 +27,41 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
+    let authObservable: Observable<AuthResponseData>;
+
     this.isLoading = true;
     if (this.isLoginMode) {
-      //...
+      authObservable = this.authService.login(email, password);
     } else {
-      this.authService.signup(email, password).subscribe(
-        (responseData) => {
-          console.log(responseData);
-          this.isLoading = false;
-        },
-        (errorMessage) => {
-          console.log(errorMessage);
-          this.error = errorMessage;
-          this.isLoading = false;
-        }
-      );
+      authObservable = this.authService.signup(email, password);
     }
+
+    // Max original (deprecated) version of subscribe:
+    // authObservable.subscribe(
+    //   (responseData) => {
+    //     console.log(responseData);
+    //     this.isLoading = false;
+    //   },
+    //   (errorMessage) => {
+    //     console.log(errorMessage);
+    //     this.error = errorMessage;
+    //     this.isLoading = false;
+    //   }
+    // );
+
+    // Not deprecated version of subscribe:
+    authObservable.subscribe({
+      next: (responseData) => {
+        console.log(responseData);
+        this.isLoading = false;
+      },
+      error: (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      },
+      complete: () => console.info('completed'),
+    });
 
     form.reset();
   }
